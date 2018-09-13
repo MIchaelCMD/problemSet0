ffdfccd32ac9aad6bfaca4ebce2b6a995e0e5194
MichaelCMD <43179251+MIchaelCMD@users.noreply.github.com>
2018-09-13 15:59:17 +0100

    Add files via upload

---
+++ src/problemSet04.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class problemSet04 {
+    public static void main(String[] args) {
+
+      int newNum = 0;
+      String newNumAS = "";
+
+        newNumAS = JOptionPane.showInputDialog("Enter a number (q to quit): ");
+
+        newNum = Integer.parseInt(newNumAS);
+
+        while(true)
+        {
+
+        }
+    }
+}
